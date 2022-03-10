d5357aea5dc3ab3654f7e9f06a0037317f348f82
Joseph-muigai <wambichojoseph@gmail.com>
2022-03-10 22:18:58 +0300

    add 101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints a string to standard error
+ *Return: program returns 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" -Dora korpar, 2015-10-19,\n", 11);
+	return (1);
+}
