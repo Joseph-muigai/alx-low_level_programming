997ffda2dfaf6d38bd71aff90122ad9ab1bd14bf
Joseph-muigai <wambichojoseph@gmail.com>
2022-03-11 21:12:06 +0300

    numebers 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - program that prints all nomers of base 10 starting from 0
+ *Return: 0 exit code of the program if successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n')
+	return (0);
+}
