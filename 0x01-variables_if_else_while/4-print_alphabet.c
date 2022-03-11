8a8452163518d004669cecdeb61ce907702d8ac3
Joseph-muigai <wambichojoseph@gmail.com>
2022-03-11 20:59:24 +0300

    alphabet game without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - program that prints the alphabet in lowercase
+ *Return: 0 exit code of the program if successful
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet == 'e' || aphabet == 'q')
+		{
+			continue;
+		}
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
