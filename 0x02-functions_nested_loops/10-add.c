6ee18f4df66a048cf96cfcdef8bfc31af5ddb40e
Joseph-muigai <wambichojoseph@gmail.com>
2022-03-16 22:12:20 +0200

    add add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add(int, int)-adds two intergers
+ * Return:result
+ * @x: interger parameter
+ * @y:interger parameter
+ */
+
+int add(int x, int y)
+{
+	int z = x + y;
+
+	return (z);
+}
