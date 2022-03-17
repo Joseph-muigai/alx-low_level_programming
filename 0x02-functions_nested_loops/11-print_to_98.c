b0be731ab033a671b5606cf960b83b468f066b80
Joseph-muigai <wambichojoseph@gmail.com>
2022-03-17 04:51:43 +0200

    add 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_to_98(int n)-prints all natural numbers from n to 98
+ * followed bya new line
+ * @n: interger parameter
+ */
+
+void print_to_98(int n);
+{
+	int i, j;
+
+	if (n >= 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+				_putchar(i);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(i);
+				_putchar('\n');
+			}
+		}
+	}
+	else if (n <= 98)
+	{
+		for (j = n; i <= 98; i++)
+		{
+			if (j != 98)
+			{
+				_putchar(j);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(i);
+				_putchar('\n');
+			}
+		}
+	}
+}
