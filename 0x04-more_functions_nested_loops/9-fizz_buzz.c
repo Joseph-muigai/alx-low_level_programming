a30bc942a79c59901e58eae31dd873509a4819e0
Joseph-muigai <wambichojoseph@gmail.com>
2022-03-18 06:56:59 +0200

    add 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
@@ -24,6 +24,10 @@ int main(void)
 		else if (i % 5 == 0  &&  i % 3 != 0)
 		{
 			printf("Buzz ");
+		}
+		else if (i == 1)
+		{
+			printf("%d ", i);
 		}
 		else
 		{
