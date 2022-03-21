9d356577e4a6e78f13507adaf0156c215b96effc
Joseph-muigai <wambichojoseph@gmail.com>
2022-03-21 16:52:14 +0300

    add 1-swapp.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int-swaps the values of two intergers
+ * @a:pointers to the intergers being swapped
+ * @b:pointers to the intergers being swapped
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
