dfddaacb142e680460905b80dee7b864f39c340a
Joseph-muigai <wambichojoseph@gmail.com>
2022-04-02 01:11:44 +0300

    a function that searches a string for any of a set of bytes.

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * _strpbrk-a function that searches a string for any of a set of bytes.
  * @s:the string to be searched
@@ -21,6 +22,6 @@ char *_strpbrk(char *s, char *accept)
 			}
 
 		}
-		return ('\0');
+		return (NULL);
 	}
 }
