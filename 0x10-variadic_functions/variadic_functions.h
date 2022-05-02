d6f4876b367f0e0998532c01db11bd1a4247022d
Joseph-muigai <wambichojoseph@gmail.com>
2022-05-02 06:10:31 +0300

    requirements check

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,2 +1,11 @@
- int _putchar(char c);
- int sum_them_all(const unsigned int n, ...);
+#ifndef VAR_FUNC_H
+#define VAR_FUNC_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif
